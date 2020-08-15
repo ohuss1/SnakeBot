5ffbfbdfd7ca9c578d5ad5a5f90e8a1bacca4603
ohuss1 <ocompu@outlook.com>
2020-08-15 16:02:21 -0400

    score board

--- Snakeauto.py
+++ Snakeauto.py
@@ -1,5 +1,17 @@
 import pyautogui
 import time
+import threading
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+int posX=130;
+int PosY=5;
+
 time.sleep(3.0);
 x,y=pyautogui.position();
 pyautogui.click(x,y);
